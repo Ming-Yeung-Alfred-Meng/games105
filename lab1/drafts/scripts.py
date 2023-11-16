f00f1138e5dd3fc29b71e5b0714fb00f901ef876
Ming_Yeung_Alfred_Meng school <alfred845301120@gmail.com>
2023-11-15 17:43:28 -0800

    jacobian_transpose() improved.

--- lab1/drafts/scripts.py
+++ lab1/drafts/scripts.py
@@ -59,13 +59,15 @@ def jacobian_transpose(links: np.ndarray,
     @return: 3m x 3 numpy array. Transpose of the Jacobian of end effector position w.r.t. all joint angles.
     """
     result = np.cross(np.eye(3), links[:, None, :]).reshape(-1, 3)
-    if root_index != -1:
-        return result
-    else:
-        return result
+    if 0 < root_index < links.shape[0]:
+        result[3 * (root_index - 1): 3 * (root_index - 1) + 3] += result[3 * root_index: 3 * root_index + 3]
+        result[3 * root_index: 3 * root_index + 3] = result[3 * (root_index - 1): 3 * (root_index - 1) + 3]
+
+    return result
 
 
 def backward(links: np.ndarray,
+             root_index: int,
              error: np.ndarray) -> np.ndarray:
     """
     Compute the gradient of the loss function i.e. L2 distance between end effector and target w.r.t. joint angles.
@@ -131,7 +133,7 @@ def link_orientations(orientations: np.ndarray,
         indices = start2end
     elif root_index == len(start2end) - 1:
         indices = start2end[:-1]
-    else: # root_index == -1
+    else:  # root_index == -1
         if parents[start2end[0]] != start2end[0]:
             indices = start2end[1:]
         else:
@@ -170,9 +172,9 @@ def link_position(joint_positions: np.ndarray,
     return joint_positions[start2end[0]] + np.sum(links[:i], axis=0)
 
 
-def gradient_descent(joint_positions: np.ndarray,
+def gradient_descent(positions: np.ndarray,
                      joint_orientations: np.ndarray,
-                     joint_parents: List[int],
+                     parents: List[int],
                      joint_offsets: np.ndarray,
                      root_index: int,
                      start2end: List[int],
@@ -180,9 +182,10 @@ def gradient_descent(joint_positions: np.ndarray,
                      learning_rate: float = 1,
                      max_iterations: int = 10,
                      max_error: float = 0.01) -> Tuple[np.ndarray, np.ndarray]:
-    orientations = link_orientations(joint_orientations, start2end, root_index)
-    links = manipulator_links(joint_positions, start2end)
-    error, error_norm = forward(joint_positions, start2end, links, target)
+    orientations = link_orientations(joint_orientations, parents,
+                                     start2end, root_index)  # the same orientation may occur twice, as desired.
+    links = manipulator_links(positions, start2end)  # include all links connect the root, as desired.
+    error, error_norm = forward(positions, start2end, links, target)
 
     iteration_count = 0
     while iteration_count < max_iterations and max_error < error_norm:
@@ -190,13 +193,13 @@ def gradient_descent(joint_positions: np.ndarray,
 
         links, orientations = step(loss_gradient, learning_rate, links, orientations)
 
-        error, error_norm = forward(joint_positions, start2end, links, target)
+        error, error_norm = forward(positions, start2end, links, target)
 
         iteration_count += 1
 
     joint_orientations = update_joint_orientations(joint_orientations, orientations, start2end, root_index)
-    new_root = link_position(joint_positions, start2end, links, root_index)
+    new_root = link_position(positions, start2end, links, root_index)
 
-    return (pose_joint_positions(new_root, joint_parents,
+    return (pose_joint_positions(new_root, parents,
                                  joint_offsets, joint_orientations),
             joint_orientations)
